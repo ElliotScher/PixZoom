a0038badf62fa9c8e2290f8496bda9340caa4651
Elliot Scher <ecscher84@gmail.com>
2023-11-26 00:17:36 -0500

    ;

--- src/components/ImageProcessingTab.tsx
+++ src/components/ImageProcessingTab.tsx
@@ -3,6 +3,10 @@ import '@/css/ImageProcessingTab.css' // Import your CSS file for styling
 
 export default function ImageProcessingTab({ onSelectFunction }: { onSelectFunction: (functionName: string) => void }) {
   const [selectedFunction, setSelectedFunction] = useState<string | null>(null)
+  const buttonWidth = 50
+  const buttonHeight = 50
+  const imageSize = 40
+
 
   function handleFunctionClick(functionName: string) {
     setSelectedFunction((prevFunction) => (prevFunction === functionName ? null : functionName))
@@ -11,26 +15,26 @@ export default function ImageProcessingTab({ onSelectFunction }: { onSelectFunct
 
   return (
     <div className='image-processing-tab'>
-      <button className={selectedFunction === 'crop' ? 'selected' : ''} onClick={() => handleFunctionClick('crop')}>
-        <img src='src\assets\crop.png' width={50} height={50} />
+      <button className={selectedFunction === 'crop' ? 'selected' : ''} onClick={() => handleFunctionClick('crop')} style={{ width: `${buttonWidth}px`, height: `${buttonHeight}px`}}>
+        <img src='src\assets\crop.png' width={imageSize} height={imageSize} />
       </button>
-      <button className={selectedFunction === 'rotate' ? 'selected' : ''} onClick={() => handleFunctionClick('rotate')}>
-        <img src='src\assets\rotate.png' width={50} height={50} />
+      <button className={selectedFunction === 'rotate' ? 'selected' : ''} onClick={() => handleFunctionClick('rotate')} style={{ width: `${buttonWidth}px`, height: `${buttonHeight}px`}}>
+        <img src='src\assets\rotate.png' width={imageSize} height={imageSize} />
       </button>
-      <button className={selectedFunction === 'resize' ? 'selected' : ''} onClick={() => handleFunctionClick('resize')}>
-        <img src='src\assets\resize.png' width={50} height={50} />
+      <button className={selectedFunction === 'resize' ? 'selected' : ''} onClick={() => handleFunctionClick('resize')} style={{ width: `${buttonWidth}px`, height: `${buttonHeight}px`}}>
+        <img src='src\assets\resize.png' width={imageSize} height={imageSize} />
       </button>
-      <button className={selectedFunction === 'brightness' ? 'selected' : ''} onClick={() => handleFunctionClick('brightness')}>
-        <img src='src\assets\brightness.png' width={50} height={50} />
+      <button className={selectedFunction === 'brightness' ? 'selected' : ''} onClick={() => handleFunctionClick('brightness')} style={{ width: `${buttonWidth}px`, height: `${buttonHeight}px`}}>
+        <img src='src\assets\brightness.png' width={imageSize} height={imageSize} />
       </button>
-      <button className={selectedFunction === 'saturation' ? 'selected' : ''} onClick={() => handleFunctionClick('saturation')}>
-        <img src='src\assets\saturation.png' width={50} height={50} />
+      <button className={selectedFunction === 'saturation' ? 'selected' : ''} onClick={() => handleFunctionClick('saturation')} style={{ width: `${buttonWidth}px`, height: `${buttonHeight}px`}}>
+        <img src='src\assets\saturation.png' width={imageSize} height={imageSize} />
       </button>
-      <button className={selectedFunction === 'contrast' ? 'selected' : ''} onClick={() => handleFunctionClick('contrast')}>
-        <img src='src\assets\contrast.png' width={50} height={50} />
+      <button className={selectedFunction === 'contrast' ? 'selected' : ''} onClick={() => handleFunctionClick('contrast')} style={{ width: `${buttonWidth}px`, height: `${buttonHeight}px`}}>
+        <img src='src\assets\contrast.png' width={imageSize} height={imageSize} />
       </button>
-      <button className={selectedFunction === 'greyscale' ? 'selected' : ''} onClick={() => handleFunctionClick('greyscale')}>
-        <img src='src\assets\greyscale.png' width={50} height={50} />
+      <button className={selectedFunction === 'greyscale' ? 'selected' : ''} onClick={() => handleFunctionClick('greyscale')} style={{ width: `${buttonWidth}px`, height: `${buttonHeight}px`}}>
+        <img src='src\assets\greyscale.png' width={imageSize} height={imageSize} />
       </button>
     </div>
   )
